738620efd6502e12ed2abc8f91023fa7f037aa15
Anshul Gupta <ansg191@anshulg.com>
2025-02-22 21:29:22 -0800

    Fix dns-admin role conflict

--- terraform/modules/gcp/direct.tf
+++ terraform/modules/gcp/direct.tf
@@ -11,10 +11,8 @@ resource "google_service_account" "dns01-solver" {
 }
 
 # Give the service account the ability to manage the DNS zone
-resource "google_project_iam_binding" "dns_admin" {
+resource "google_project_iam_member" "dns_admin" {
   project = data.google_project.default.project_id
   role    = "roles/dns.admin"
-  members = [
-    "serviceAccount:${google_service_account.dns01-solver.email}"
-  ]
+  member  = "serviceAccount:${google_service_account.dns01-solver.email}"
 }
