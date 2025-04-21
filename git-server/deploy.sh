a8662d989e47585dbcb04572cba8f89df9705bb9
Anshul Gupta <ansg191@anshulg.com>
2025-04-21 13:49:16 -0700

    Use system ssh instead of gcloud for git-server

--- git-server/deploy.sh
+++ git-server/deploy.sh
@@ -2,28 +2,18 @@
 
 set -eu
 
-ZONE="us-west2-b"
 USER="anshulgupta"
-INSTANCE="git-instance"
+INSTANCE="git.anshulg.com"
 SERVER="$USER@$INSTANCE"
 
-echo "Deploying to $SERVER in $ZONE"
+echo "Deploying to $SERVER..."
 
 echo "::group::Copying files to server"
-
-gcloud compute scp --zone=$ZONE Caddyfile $SERVER:Caddyfile
-gcloud compute scp --zone=$ZONE cgitrc $SERVER:cgitrc
-gcloud compute scp --zone=$ZONE setup.sh $SERVER:setup.sh
-gcloud compute scp --zone=$ZONE config.ini $SERVER:config.ini
-gcloud compute scp --zone=$ZONE notion-df.py $SERVER:notion-df.py
-gcloud compute scp --zone=$ZONE firewall.conf $SERVER:firewall.conf
-gcloud compute scp --zone=$ZONE update-blocklists.sh $SERVER:update-blocklists.sh
-gcloud compute ssh --zone=us-west2-b $SERVER -- 'rm -rf git-shell-commands'
-gcloud compute scp --zone=$ZONE --recurse git-shell-commands/ $SERVER:git-shell-commands
+scp -r ./ $SERVER:~
 echo "::endgroup::"
 
 echo "Running setup script on $SERVER"
-gcloud compute ssh --zone=$ZONE $SERVER << EOF
+ssh $SERVER << EOF
 set -eux
 chmod +x setup.sh
 bash setup.sh
