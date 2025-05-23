8c462fffa910e2efb30958738febd33362e01043
Anshul Gupta <ansg191@anshulg.com>
2025-05-23 14:01:12 -0700

    Add `reprepro` apt repo bucket

--- terraform/modules/gcp/buckets.tf
+++ terraform/modules/gcp/buckets.tf
@@ -173,3 +173,40 @@ resource "google_storage_bucket_iam_member" "cnpg-backup-paperless-object" {
 }
 
 # endregion Paperless Cluster Backups
+
+# region Debian Apt Repository Bucket
+resource "google_storage_bucket" "debian-apt-repo" {
+	name          = "anshulg-debian-apt"
+	location      = "US"
+	force_destroy = false
+
+	uniform_bucket_level_access = true
+	storage_class               = "STANDARD"
+	public_access_prevention    = "enforced"
+}
+
+resource "google_service_account" "debian-apt-repo" {
+	account_id = "debian-apt-repo"
+}
+
+resource "google_storage_bucket_iam_member" "debian-apt-repo-bucket" {
+	bucket = google_storage_bucket.debian-apt-repo.name
+	member = "serviceAccount:${google_service_account.debian-apt-repo.email}"
+	role   = "roles/storage.legacyBucketWriter"
+}
+
+resource "google_storage_bucket_iam_member" "debian-apt-repo-object" {
+	bucket = google_storage_bucket.debian-apt-repo.name
+	member = "serviceAccount:${google_service_account.debian-apt-repo.email}"
+	role   = "roles/storage.legacyObjectOwner"
+}
+
+resource "google_service_account_iam_binding" "debian-apt-repo" {
+	service_account_id = google_service_account.debian-apt-repo.id
+	role               = "roles/iam.workloadIdentityUser"
+	members = [
+		"serviceAccount:${data.google_project.default.project_id}.svc.id.goog[reprepro/reprepro]"
+	]
+}
+
+# endregion Debian Apt Repository Bucket
