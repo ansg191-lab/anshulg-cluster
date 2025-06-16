7d56a7f143a343c1d276c52bd9066ca861cd7123
Anshul Gupta <ansg191@anshulg.com>
2025-06-16 16:29:36 -0700

    Remove intermediate certificate

--- terraform/modules/gcp/ca.tf
+++ terraform/modules/gcp/ca.tf
@@ -53,55 +53,6 @@ resource "google_privateca_certificate_authority" "default" {
   lifetime = "315360000s"
 }
 
-resource "google_privateca_certificate_authority" "subordinate" {
-  pool                     = google_privateca_ca_pool.default.name
-  certificate_authority_id = "anshul-sub-ca-1"
-  location                 = "us-west1"
-  deletion_protection      = false
-  type                     = "SUBORDINATE"
-  subordinate_config {
-    certificate_authority = google_privateca_certificate_authority.default.name
-  }
-  config {
-    subject_config {
-      subject {
-        common_name  = "Anshul Gupta Intermediate CA"
-        organization = "Anshul Gupta"
-        province     = "California"
-        country_code = "US"
-      }
-    }
-    x509_config {
-      ca_options {
-        is_ca = true
-      }
-      key_usage {
-        base_key_usage {
-          digital_signature  = true
-          content_commitment = true
-          key_encipherment   = true
-          data_encipherment  = true
-          key_agreement      = true
-          cert_sign          = true
-          crl_sign           = true
-          decipher_only      = true
-        }
-        extended_key_usage {
-          server_auth      = true
-          client_auth      = true
-          email_protection = true
-          code_signing     = true
-          time_stamping    = true
-        }
-      }
-    }
-  }
-  key_spec {
-    algorithm = "EC_P384_SHA384"
-  }
-  lifetime = "157680000s"
-}
-
 resource "google_service_account" "sa-google-cas-issuer" {
   account_id = "sa-google-cas-issuer"
 }
