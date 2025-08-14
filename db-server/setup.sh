f6a357fdc8a153e00622e3e91d9ea7aa36042595
Anshul Gupta <ansg191@anshulg.com>
2025-08-13 17:30:16 -0700

    Add `zeyple` to rpi4 to encrypt & sign emails

--- db-server/setup.sh
+++ db-server/setup.sh
@@ -116,6 +116,52 @@ EOF
 	systemctl enable postfix
 }
 
+setup_zeyple() {
+	log "Setting up Zeyple..."
+
+	# Create zeyple user if it doesn't exist
+	if ! id -u zeyple >/dev/null 2>&1; then
+		log "Adding Zeyple user..."
+		adduser --system --no-create-home --disabled-login zeyple
+	fi
+
+	# Create Zeyple directories
+	chmod 700 /var/lib/zeyple/keys
+	chown -R zeyple: /var/lib/zeyple/keys
+
+	touch /var/log/zeyple.log && chown zeyple: /var/log/zeyple.log
+
+	# Download Zeyple
+	log "Downloading Zeyple..."
+	wget -qO /usr/local/bin/zeyple "https://github.com/ansg191/zeyple/raw/refs/heads/signing/zeyple/zeyple.py"
+	chmod 744 /usr/local/bin/zeyple
+	chown zeyple: /usr/local/bin/zeyple
+
+	# Modify postfix master.cf to use Zeyple
+	log "Configuring Postfix to use Zeyple..."
+	read -r -d '' POSTFIX_CF <<EOF || true
+zeyple    unix  -       n       n       -       -       pipe
+  user=zeyple argv=/usr/local/bin/zeyple \${sender} \${recipient}
+
+localhost:10026 inet  n       -       n       -       10      smtpd
+  -o content_filter=
+  -o receive_override_options=no_unknown_recipient_checks,no_header_body_checks,no_milters
+  -o smtpd_helo_restrictions=
+  -o smtpd_client_restrictions=
+  -o smtpd_sender_restrictions=
+  -o smtpd_recipient_restrictions=permit_mynetworks,reject
+  -o mynetworks=127.0.0.0/8,[::1]/128
+  -o smtpd_authorized_xforward_hosts=127.0.0.0/8,[::1]/128
+EOF
+	append_file /etc/postfix/master.cf "$POSTFIX_CF"
+
+	# Modify postfix main.cf to use Zeyple
+	postconf -e "content_filter = zeyple"
+
+	log "Restarting Postfix..."
+	systemctl restart postfix
+}
+
 setup_issuer() {
 	log "Setting up Google CAS issuer..."
 
@@ -298,6 +344,7 @@ install_packages
 install_ca
 setup_firewall
 setup_mta
+setup_zeyple
 setup_issuer
 setup_postgres
 install_restic
