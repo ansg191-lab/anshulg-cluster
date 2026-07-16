ae21ea208191c46b207669a96f33e478c27cd104
Anshul Gupta <ansg191@anshulg.com>
2026-07-15 20:17:49 -0700

    Add explicit policy for admins

--- terraform/modules/vault/groups.tf
+++ terraform/modules/vault/groups.tf
@@ -4,11 +4,16 @@
 resource "vault_identity_group" "vault_admins" {
   name     = "vault-admins"
   type     = "external"
-  policies = ["tfc-policy"]
+  policies = [vault_policy.admin_policy.name]
 }
 
-resource "vault_identity_entity_alias" "vault_admins_oidc" {
-  name           = "idm_admins"
+resource "vault_identity_group_alias" "vault_admins_oidc" {
+  name           = "idm_admins@auth.anshulg.com"
   mount_accessor = vault_jwt_auth_backend.oidc.accessor
   canonical_id   = vault_identity_group.vault_admins.id
+}
+
+resource "vault_policy" "admin_policy" {
+  name   = "admin"
+  policy = file("${path.module}/policies/admin.hcl")
 }
